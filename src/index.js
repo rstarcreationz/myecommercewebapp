110abddda207876c5b0eaeb71734e9f65e82016d
rstarcreationz <ajay.cloudwapp@gmail.com>
2021-01-28 11:15:27 +0530

    link update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter,Switch, Route} from "react-router-dom";
 import './index.css';
 import App from './App';
 import swDev from './swDev';
-import Footer from './components/Footer';
 import { Provider } from "react-redux";
 import rootReducer from './Redux/index';
 import {createStore} from 'redux';
+import Home from "./components/HomePage/Home";
+import About from "./components/AboutPage/About";
+import Contact from "./components/ContactPage/Contact";
+import Login from "./components/LoginPage/login";
+import Register from "./components/LoginPage/Register";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import Footer from './components/Footer';
 
 const store=createStore(rootReducer)
 
@@ -15,10 +21,43 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
     <Provider store={store}>
-    <App />
+      <App/>
+      <Switch>
+          <Route exact path="/">
+            <ProtectedRoutes Cmp={Home} />
+          </Route>
+
+          <Route exact path="/Home">
+            <ProtectedRoutes Cmp={Home} />
+          </Route>
+
+          <Route exact path="/About">
+            <ProtectedRoutes Cmp={About} />
+          </Route>
+
+          <Route exact path="/Contact">
+            <ProtectedRoutes Cmp={Contact} />
+          </Route>
+
+          {/* <Route exact path="/User/edit_user/:id" component={EditUser} />
+      <Route exact path="/User/view_user/:id" component={ViewUser} />  */}
+
+          <Route exact path="/Signup" component={Register} />
+          
+
+          <Route exact path="/login" component={Login} />
+          
+
+
+          {/* <Route exact path="/login" component={Login} />
+      <Route exact path="/Signup" component={Register} /> */}
+          {/* <Route exact path="/book" component={Book} /> */}
+
+
+
+        </Switch>
+        <Footer/>
     </Provider>
-    
-    <Footer/>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
